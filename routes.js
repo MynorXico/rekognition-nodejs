a72cf027b73b49cafda4ac7f42c5363c7c257fef
Mynor Xico <xic14659@uvg.edu.gt>
2020-07-11 22:59:30 -0600

    Healthcheck endpoint

--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var emotionController = require("./Controllers/EmotionController");
 
 router.post("/getEmotions", emotionController.getEmotion);
-
+router.get("/", (req, res) => {
+    res.send({"api":"rekognition"});
+});
 module.exports = router;
